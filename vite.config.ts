01161535f343a639c73015d7a4966b99aa059788
hayden <hayden.mike.zhang@gmail.com>
2023-02-28 17:02:41 +0800

    fix: fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import { defineConfig } from "vite";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/demo-report-chart/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vueJsx({})],
-});
+})
